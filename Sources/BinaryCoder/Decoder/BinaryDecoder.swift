8276159d9d0eefd7bfed8b0d4fe099d36e54b78b
Luke Howard <lukeh@padl.com>
2023-06-16 16:13:42 +1000

    remove debugging

--- Sources/BinaryCoder/Decoder/BinaryDecoder.swift
+++ Sources/BinaryCoder/Decoder/BinaryDecoder.swift
@@ -15,7 +15,6 @@ public struct BinaryDecoder {
     public func decode<Value>(_ type: Value.Type, from data: Data) throws -> Value where Value: Decodable {
         let state = BinaryDecodingState(config: config, data: data)
         var count: Int? = nil
-        debugPrint("decode type \(type)")
         if type is any ArrayRepresentable.Type, config.variableSizedTypeStrategy == .lengthTaggedArrays {
             // propagate array count to unkeyed container count
             count = try Int(UInt16(from: BinaryDecoderImpl(state: state, codingPath: [])))
